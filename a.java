0561ea96c6bdcfc825579229beb8e5ef6ea1d1b4
Saravanan Ganesan <gsaravg@gmail.com>
2022-12-27 16:12:06 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,28 @@
+package com.fedex.rise.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(1)
+public class SpringContext implements ApplicationContextAware {
+
+	private static ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+	
+	public static Object getBean(String beanName) {
+		
+		return getApplicationContext().getBean(beanName);
+	}
+}
